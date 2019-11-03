118c4849eaa02bb7846b4b4a82ae1dfe812a8c5d
Huxifan <xifan_1995@163.com>
2019-11-03 18:42:00 +0800

    富文本编辑器

--- booktest/urls.py
+++ booktest/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url, include
 from booktest import views
 urlpatterns = [
     url(r'^index$', views.index),
+    url(r'^editor$', views.editor),
 ]
--- test6/settings.py
+++ test6/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 TINYMCE_DEFAULT_CONFIG = {
-    'theme': 'advance',
-    'width': 600,
-    'height': 400
+    'theme': 'advanced',
+    'width': 400,
+    'height': 100
 }
